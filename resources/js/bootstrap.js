584e8291f97aa9cc424e572f5a91b0ae89069a4a
RubenCano21 <ruben217.rc@gmail.com>
2025-11-27 23:29:55 -0400

    feat: Update login form submission to include CSRF token generation; adjust remember me functionality in form transformation

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,9 +3,13 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL || '';
+// Si tu proyecto está en subcarpeta, ajusta la baseURL automáticamente
+window.axios.defaults.baseURL = '/inf513/grupo14sa/TiendaCelestinaWeb/public';
 
-// Token CSRF
+// Muy importante:
+window.axios.defaults.withCredentials = true;
+
+// Token CSRF desde meta
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
@@ -13,3 +17,6 @@ if (token) {
 } else {
     console.error('CSRF token not found');
 }
+
+// *** Línea obligatoria para Inertia y login ***
+window.axios.get('/sanctum/csrf-cookie');
